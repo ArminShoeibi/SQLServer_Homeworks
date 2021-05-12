da0c094cc87ddb3c6b2eafd3899f96fa1b4a5284
Armin Shoeibi <68565441+ArminShoeibi@users.noreply.github.com>
2021-05-12 14:39:52 +0430

    Create session4.sql
    Homework for Session4 (CREATE & INSERT)

---
+++ session4.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE SAPERP COLLATE Persian_100_CI_AS;
+GO
+
+USE SAPERP
+GO
+
+CREATE SCHEMA Purchasing
+GO
+
+CREATE SCHEMA Sales
+GO
+
+CREATE SCHEMA Finance
+GO
+
+CREATE TABLE SAPERP.Sales.Customers
+(
+CustomerId int PRIMARY KEY IDENTITY(1,1),
+FirstName nvarchar(30) NOT NULL,
+LastName nvarchar(30) NOT NULL,
+BirthDate date NOT NULL,
+PhoneNumber nvarchar(11),
+Email nvarchar(100),
+);
+
+GO
+
+INSERT INTO Sales.Customers 
+VALUES
+('Armin','Shoeibi','2001-05-01','09','rmin@cyberservices.com'),
+('Eren','Yeager','2013-04-07','','');
